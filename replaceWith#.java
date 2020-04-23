01397bc53495c476552c547c3e3118264ef45299
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-04-23 19:54:47 +0530

    Update and rename replaceWith#.txt to replaceWith#.java

--- replaceWith#.java
+++ replaceWith#.java
@@ -1,11 +1,11 @@
-Implement a function that replaces each digit in the given string with a '#' character.
+/*Implement a function that replaces each digit in the given string with a '#' character.
 
 Example
 
 For input = "There are 12 points", the output should be
 replaceAllDigitsRegExp(input) = "There are ## points".
+*/
 
-Code :
 
 String replaceAllDigitsRegExp(String input) {
     var sb= new StringBuilder();
