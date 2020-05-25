d05f48ed8f45d6319750fbaf8e5a2435d7dc7499
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-05-25 22:20:09 +0530

    Update movieDirectors.sql

--- movieDirectors.sql
+++ movieDirectors.sql
@@ -52,4 +52,16 @@ END
 
 */
 
+/*Please add ; after each select statement*/
+CREATE PROCEDURE movieDirectors()
+BEGIN
+SELECT director
+FROM moviesInfo
+WHERE year >2000
+GROUP BY director
+HAVING SUM(oscars)>2
+ORDER BY director;
+	
+END
+
 
